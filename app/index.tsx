4baa51e84d5cf99461ad031309ceb904d5d328b4
alfian0 <alfian.official.mail@gmail.com>
2025-03-14 14:55:50 +0700

    add react-native-papper

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,44 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, Button, FlatList, View, Image } from "react-native";
 import { useIndex } from "./index.hooks";
+import { Card, Text } from "react-native-paper";
 
 export default function Index() {
   const { data, error, isLoading, refetch } = useIndex();
 
-  console.log(isLoading);
-  console.log(data);
+  if (isLoading) {
+    return (
+      <View>
+        <ActivityIndicator />
+        <Text>Loading characters...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>Failed to load data</Text>
+        <Button title="Retry" onPress={refetch} />
+      </View>
+    );
+  }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Card>
+            <Image source={{ uri: item.image }} width={100} height={100} />
+            <Card.Content>
+              <Text>{item.name}</Text>
+              <Text>{item.species}</Text>
+              <Text>{item.location.name}</Text>
+            </Card.Content>
+          </Card>
+        )}
+      />
     </View>
   );
 }
