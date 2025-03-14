5172949d77385f6db17b6378745e08bafc3f21ba
alfian0 <alfian.official.mail@gmail.com>
2025-03-15 04:00:36 +0700

    useMemo + useCallback

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,11 @@ import {
   View,
   Image,
   Dimensions,
+  ListRenderItem,
 } from "react-native";
 import { Card, Text, Button } from "react-native-paper";
 import useIndex from "./index.hooks";
+import { useCallback, useMemo } from "react";
 
 const numColumns = 2; // Number of columns in grid
 const screenWidth = Dimensions.get("window").width;
@@ -15,6 +17,29 @@ const cardWidth = screenWidth / numColumns - 16; // Adjust spacing
 export default function Index() {
   const { data, error, isLoading, isFetchingMore, refetch, loadMore } =
     useIndex();
+
+  const keyExtractor = useMemo(
+    () => (item: any, index: number) => String(index),
+    []
+  );
+
+  const renderItem: ListRenderItem<any> = useCallback(({ item }) => {
+    return (
+      <View style={{ width: cardWidth, marginVertical: 8 }}>
+        <Card>
+          <Card.Content>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: "100%", height: 120, borderRadius: 8 }}
+            />
+            <Text variant="titleMedium">{item.name}</Text>
+            <Text>{item.species}</Text>
+            <Text>{item.location.name}</Text>
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  }, []);
 
   if (isLoading) {
     return (
@@ -40,27 +65,14 @@ export default function Index() {
     <View className="flex-1">
       <FlatList
         data={data}
-        keyExtractor={(item, index) => String(index)}
+        keyExtractor={keyExtractor}
         numColumns={numColumns} // Enable grid layout
         columnWrapperStyle={{
           justifyContent: "space-between",
           paddingHorizontal: 8,
         }} // Evenly distribute columns
-        renderItem={({ item }) => (
-          <View style={{ width: cardWidth, marginVertical: 8 }}>
-            <Card>
-              <Card.Content>
-                <Image
-                  source={{ uri: item.image }}
-                  style={{ width: "100%", height: 120, borderRadius: 8 }}
-                />
-                <Text variant="titleMedium">{item.name}</Text>
-                <Text>{item.species}</Text>
-                <Text>{item.location.name}</Text>
-              </Card.Content>
-            </Card>
-          </View>
-        )}
+        renderItem={renderItem}
+        initialNumToRender={10} // Optimize initial render
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
         ListFooterComponent={
